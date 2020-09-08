bd10a8ee124bc449932b9be9577432bda2ce1a32
gobackk1 <k.kwbt@icloud.com>
2020-09-09 06:54:17 +0900

    グローバルスタイル作成

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from '@/components'
 import * as serviceWorker from './serviceWorker'
+import { Global } from '@emotion/core'
+import { css } from '@emotion/core'
+import { reset, global } from '@/styles'
+
+export const CSS = {
+  global: css`
+    ${reset}
+    ${global}
+  `
+}
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={CSS['global']} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/styles/index.ts
@@ -0,0 +1,2 @@
+export { reset } from './modules/reset'
+export { global } from './modules/global'
