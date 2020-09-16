57b5925de24c0c638577dd661be7b8d1f89b168a
gobackk1 <k.kwbt@icloud.com>
2020-09-16 22:36:52 +0900

    コンフリク解消の時にデグレしてしまったので

--- src/components/views/Board.tsx
+++ src/components/views/Board.tsx
@@ -3,13 +3,15 @@ import { useParams, useHistory } from 'react-router-dom'
 import { fetchBoards, archiveBoard } from '@/scripts/redux/state/board/actions'
 import { useSelector, useDispatch } from 'react-redux'
 import * as I from '@/scripts/interfaces'
-import { LoadingSpinner } from '../common/LoadingSpinner'
+import { LoadingSpinner, CardList } from '@/components'
 import { useSnackbarContext } from '@/scripts/hooks'
 import { css } from '@emotion/core'
 import { BoardTitle } from '@/components'
 import { Button } from '@material-ui/core'
 import { Drawer, makeStyles } from '@material-ui/core'
 import { MoreHoriz } from '@material-ui/icons'
+import { Theme } from '@material-ui/core'
+import { fetchList, createList } from '@/scripts/redux/state/list/actions'
 
 /**
  * ボードの View, 各種操作を管理する
@@ -17,13 +19,24 @@ import { MoreHoriz } from '@material-ui/icons'
 export const Board: React.FC = () => {
   const boardState = useSelector((state: I.ReduxState) => state.board)
   const userState = useSelector((state: I.ReduxState) => state.user)
+  const listState = useSelector((state: I.ReduxState) => state.list)
   const dispatch = useDispatch()
   const { showSnackbar } = useSnackbarContext()
   const [open, setOpen] = React.useState(false)
   const muiStyle = useStyles()
   const history = useHistory()
+  const { boardId } = useParams<{ boardId: string }>()
 
-  const params = useParams<{ boardId: string }>()
+  React.useEffect(() => {
+    ;(async () => {
+      if (!userState.user) return
+      try {
+        await dispatch(fetchList({ boardId }))
+      } catch ({ message }) {
+        showSnackbar({ message, type: 'error' })
+      }
+    })()
+  }, [userState, dispatch, fetchList, showSnackbar, boardId])
 
   /**
    * ユーザーがログインしていたら、ボード一覧を取得
@@ -48,15 +61,19 @@ export const Board: React.FC = () => {
   }
 
   const onClickArchive = async () => {
-    if (!params.boardId) return
+    if (!boardId) return
     if (!window.confirm('ボードをアーカイブしてもよろしいですか？')) return
     try {
-      await dispatch(archiveBoard({ id: params.boardId }))
+      await dispatch(archiveBoard({ id: boardId }))
       setOpen(false)
       history.push('/boards')
     } catch ({ message }) {
       showSnackbar({ message, type: 'error' })
     }
+  }
+
+  const onClick = () => {
+    if (boardId) dispatch(createList({ title: 'new card', boardId }))
   }
 
   return (
@@ -66,11 +83,24 @@ export const Board: React.FC = () => {
         <>
           {boardState.boards.length ? (
             <>
-              <BoardTitle />
+              <div css={styles['board-header']}>
+                <BoardTitle />
+              </div>
               {boardState.error && (
                 <>エラーメッセージ{boardState.error.message}</>
               )}
-              <div>ここにリストを並べる</div>
+              <Button onClick={onClick}>create list</Button>
+              <ul css={styles['card-list-container']}>
+                {boardId &&
+                  listState.boards[boardId] &&
+                  listState.boards[boardId].lists.map((list, i) => {
+                    return (
+                      <li key={i}>
+                        <CardList list={list} />
+                      </li>
+                    )
+                  })}
+              </ul>
             </>
           ) : (
             'まだボードがありません。「ボード一覧」から新しいボードを作成してください。'
@@ -132,5 +162,11 @@ const styles = {
     left: -76%;
     visibility: visible;
   `,
-  'drawer-content': css``
+  'drawer-content': css``,
+  'board-header': (theme: Theme) => css`
+    margin-bottom: ${theme.spacing(1)}px;
+  `,
+  'card-list-container': css`
+    display: flex;
+  `
 }
