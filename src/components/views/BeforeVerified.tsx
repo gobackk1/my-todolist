e2c88b6e07001aeda51cc5ff87d0a6e91d833702
gobackk1 <k.kwbt@icloud.com>
2020-10-31 18:18:57 +0900

    BeforeVerifiedコンポーネントの見た目調整

--- src/components/views/BeforeVerified.tsx
+++ src/components/views/BeforeVerified.tsx
@@ -1,37 +1,45 @@
 import React from 'react'
-import firebase from 'firebase'
-import { useSnackbarContext } from '@/scripts/hooks'
-import { LoadingSpinner } from '@/components'
+import { useFirebase } from '@/scripts/hooks'
+import { LoadingSpinner, PageContainer } from '@/components'
 import { useSelector } from 'react-redux'
-import { OPTION } from '@/option'
+import { Button, makeStyles, Typography } from '@material-ui/core'
+import { theme } from '@/styles'
 
 export const BeforeVerified: React.FC = () => {
-  const { showSnackbar } = useSnackbarContext()
-  const currentUserState = useSelector(state => state.currentUser)
-
-  const onClick = () => {
-    const user = firebase.auth().currentUser
-    if (user) {
-      user.sendEmailVerification({
-        url: OPTION.URL_AFTER_EMAIL_CONFIRMATION
-      })
-      showSnackbar({
-        message: OPTION.MESSAGE.AUTH.SEND_EMAIL_VERIFICATION,
-        type: 'info'
-      })
-    }
-  }
+  const { isLoggingIn } = useSelector(state => state.currentUser)
+  const { isResendEmailDisabled, resendEmailVerification } = useFirebase()
+  const styles = useStyles()
 
   return (
-    <div>
-      {currentUserState.isLoggingIn ? (
-        <LoadingSpinner />
-      ) : (
-        <>
-          メールアドレスの確認が終わっていません。受信メールを確認し、アカウントを有効化してから画面を更新してください
-          <button onClick={onClick}>確認用メールを再送信する</button>
-        </>
-      )}
+    <div className={`AppBeforeVerified-root ${styles.root}`}>
+      <PageContainer>
+        {isLoggingIn ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            <Typography>
+              メールアドレスの確認が終わっていません。受信メールを確認し、アカウントを有効化してから画面を更新してください。
+            </Typography>
+            <Button
+              color="primary"
+              onClick={resendEmailVerification}
+              variant="contained"
+              disabled={isResendEmailDisabled}
+            >
+              確認用メールを再送信する
+            </Button>
+          </>
+        )}
+      </PageContainer>
     </div>
   )
 }
+
+const useStyles = makeStyles({
+  root: {
+    paddingTop: theme.spacing(10),
+    '& .MuiTypography-root': {
+      marginBottom: theme.spacing(3)
+    }
+  }
+})
